a12f47956cfe3a0c292e28f5479d69e77d462cc6
alberto.floresr111 <alberto.floresr111@gmail.com>
2019-06-02 18:22:38 -0500

    First example of integration  with Kafka & Apache Camel INBOX_PATH

--- src/main/java/com/moneygram/demo/springbootcamelkafkaintegration/route/SimpleRouteBuilder.java
+++ src/main/java/com/moneygram/demo/springbootcamelkafkaintegration/route/SimpleRouteBuilder.java
@@ -7,19 +7,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class SimpleRouteBuilder extends RouteBuilder {
 
-    private static final String KAFKA_SERVER  = "kafka://localhost:9092?";
-    private static final String TOPIC_NAME    = "topic=testing";
-    private static final String BROKER_NAME   = "&brokers=localhost:9092";
+    private static final String KAFKA_SERVER    = "kafka://localhost:9092?";
+    private static final String TOPIC_NAME      = "topic=testing";
+    private static final String BROKER_NAME     = "&brokers=localhost:9092";
+    private static final String INBOX_PATH      = "/home/aguilas/IdeaProjects/spring-boot-camel-kafka-integration/src/main/resources";
+
 
     @Override
     public void configure() throws Exception {
 
         String toKafkaUri = KAFKA_SERVER + TOPIC_NAME + BROKER_NAME;
 
-        from("file:/home/aguilas/IdeaProjects/spring-boot-camel-kafka-integration/src/main/resources/inbox?noop=true")
+        from("file:" + INBOX_PATH + "/inbox?noop=true")
                 .split()
                 .tokenize("\n")
                 .bean( new MyTransformer(), "transformContent")
                 .to(toKafkaUri);
+
     }
 }
